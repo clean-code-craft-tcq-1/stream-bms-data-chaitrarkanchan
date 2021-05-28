de8130f889f13791ab25be4d54d9b898b47bcabc
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-05-28 19:57:07 +0530

    Update BMSSender.java

--- src/main/java/BMSStreamSender/BMSSender.java
+++ src/main/java/BMSStreamSender/BMSSender.java
@@ -3,15 +3,9 @@ package BMSStreamSender;
 public class BMSSender {
 
 	public static void main(String[] args) {
-		while(true) {
-		System.out.println("Please enter number of BMS readings to be generate (Min->10):");
-		try {
-		String inputGenarateRange = System.console().readLine();
-		if(inputGenarateRange.contentEquals("x"))
-		{
-			break;
-		}
 		
+		System.out.println("Please enter number of BMS readings to be generate (Min->10):");
+		String inputGenarateRange = System.console().readLine();
 		if(ValidateUserInput.isNumeric(inputGenarateRange)&&ValidateUserInput.isGreaterthanMin(inputGenarateRange))
 		{
 			System.out.println("You entered string " + Integer.parseInt(inputGenarateRange));
@@ -19,14 +13,11 @@ public class BMSSender {
 			bms_s.genearateBatteryReadings( Integer.parseInt(inputGenarateRange));
 			bms_s.printBatteryReadingsToConsole();
 		}
-		System.out.println("\n\n Enter 'x' to exit...! ");
+		
 		
 		}
-		catch(NullPointerException  e)
-		{
-			System.out.println(e.getMessage());
-		}
-		}
+		
+		
 		
 	}
-}
+
