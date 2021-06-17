0711115010044f0d27787acfbe540e3ea6dd35ba
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-17 20:51:13 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -4,7 +4,8 @@ import subprocess
 
 
 output = subprocess.call(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'])
-numlist = int(output(10))
+numlists = output(10)
+numlist = int(numlists)
 window_size = 3
 num_series = pd.Series(numlist)
 windows = num_series.rolling(window_size)
