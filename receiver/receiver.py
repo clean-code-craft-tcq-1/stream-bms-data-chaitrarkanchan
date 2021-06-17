ddece33680c493517e72af9e7402602af79f038b
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-17 18:49:51 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -8,6 +8,6 @@ windows = num_series.rolling(window_size)
 
 moving_averages = windows.mean()
 moving_averages = moving_averages.tolist()
-moving_averages_without_nans = moving_averages_list[window_size - 1:]
+moving_averages_without_nans = moving_averages[window_size - 1:]
 
 print(moving_averages_without_nans)
