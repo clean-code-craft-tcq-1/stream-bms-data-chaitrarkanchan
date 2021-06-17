28b006258b560f1eeee8e3f2c90b51dc63532447
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-17 18:39:01 +0530

    Create receiver.py

---
+++ receiver/receiver.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import bms_sender_data
+
+numlist = bms_sender_data.streaming_bms_readings(5)
+window_size = 3
+num_series = pd.Series(numlist)
+windows = num_series.rolling(window_size)
+
+moving_averages = windows.mean()
+moving_averages = moving_averages.tolist()
+moving_averages_without_nans = moving_averages_list[window_size - 1:]
+
+print(moving_averages_without_nans)
