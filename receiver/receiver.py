d536af9ade455d372f0d9d4f3f690e8b7a05a237
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-18 10:17:49 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,33 +1,6 @@
 import pandas as pd
 import subprocess
 
-output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'],stdout=subprocess.PIPE)
+output =  subprocess.check_output(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'],stdout=subprocess.PIPE)
 num = output.stdout.read()
 print(num)
-numlist = input()
-window_size = 3
-num_series = pd.Series(numlist)
-windows = num_series.rolling(window_size)
-
-moving_averages = windows.mean()
-moving_averages = moving_averages.tolist()
-moving_averages_without_nans = moving_averages[window_size - 1:]
-
-def max_temp(numlist):
-    return max(numlist)
-      
-def min_temp(numlist):
-    return min(numlist) 
-
-def max_soc(numlist):
-    return max(numlist)  
-
-def min_soc(numlist):
-    return min(numlist)
-
-print(moving_averages_without_nans)
-print("MaxTemp - ",max_temp(numlist),"  MinTemp - ",min_temp(numlist))
-print("MaxSOC -  ",max_soc(numlist),"  MinSOC -  ",min_soc(numlist))
-    
-
-    
