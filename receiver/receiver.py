b44f31d1b834eb69517f50735f5430d6bc4cd354
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-18 12:48:19 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -8,5 +8,7 @@ def moving_average(x, w):
   num = output.stdout.read()
   df = pd.DataFrame(num)
   for i in range(0,df.shape[0]-2):
-    df.loc[df.index[i+2],'SMA_3'] = np.round(((df.iloc[i,1]+ df.iloc[i+1,1] +df.iloc[i+2,1])/3),1)
+    df['pandas_SMA_3'] = df.iloc[:,1].rolling(window=3).mean()
+    df.to_numpy().max()
+    df.to_numpy().min()
     df.head()
