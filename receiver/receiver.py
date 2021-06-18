ed465cfe6699fcb879f070290aaaa4ebb5c67ccd
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-18 10:23:05 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import subprocess
 
-output =  subprocess.check_output(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'])
-num = output.stdout.read()
-print(num)
+output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'],stdout=subprocess.PIPE)
+print(output.stdout.read())
