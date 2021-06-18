2e80af129882d348d8c3a01a699fb807f075de2f
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-18 11:57:05 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -11,23 +11,4 @@ windows = num_series.rolling(window_size)
 moving_averages = windows.mean()
 moving_averages = moving_averages.tolist()
 moving_averages_without_nans = moving_averages[window_size - 1:]
-
-def max_temp(numlist):
-    maximumtemp = max(numlist)
-    print(maximumtemp) 
-      
-def min_temp(numlist):
-    minimumtemp = min(numlist) 
-    print(minimumtemp)
-
-def max_soc(numlist):
-    maximumsoc = max(numlist)
-    print(maximumsoc)
-
-def min_soc(numlist):
-    minimumsoc = min(numlist)
-    print(minimumsoc)
-
 print(moving_averages_without_nans)
-print("MaxTemp - ",max_temp(numlist),"  MinTemp - ",min_temp(numlist))
-print("MaxSOC -  ",max_soc(numlist),"  MinSOC -  ",min_soc(numlist))
