e09fecc190be93e620bb8599e478dd8eb48f6f94
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-18 12:44:04 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 import subprocess
 
@@ -5,9 +6,7 @@ import subprocess
 def moving_average(x, w):
   output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'],stdout=subprocess.PIPE)
   num = output.stdout.read()
-  return np.convolve(x, np.ones(w), 'valid') / w
-
-data = np.array(num)
-
-print(moving_average(data,4))
-print ("Max number:",max(data), "\nMin number:",min(data))
+  df = pd.DataFrame(num)
+  for i in range(0,df.shape[0]-2):
+    df.loc[df.index[i+2],'SMA_3'] = np.round(((df.iloc[i,1]+ df.iloc[i+1,1] +df.iloc[i+2,1])/3),1)
+    df.head()
