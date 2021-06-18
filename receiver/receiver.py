ded723a52e66504568ce4cf13aa7699d52d51b6d
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-18 12:38:01 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,15 +1,13 @@
-import pandas as pd
+import numpy as np
 import subprocess
 
-output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'],stdout=subprocess.PIPE)
-num = output.stdout.read()
-numlist = pd.to_numeric(num)
-window_size = 3
-num_series = pd.Series(numlist)
-windows = num_series.rolling(window_size)
 
-moving_averages = windows.mean()
-moving_averages = moving_averages.tolist()
-moving_averages_without_nans = moving_averages[window_size - 1:]
-print(moving_averages_without_nans)
-print ("Max number:",max(numlist), "\nMin number:",min(numlist))
+def moving_average(x, w):
+  output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSServiceImpl.java'],stdout=subprocess.PIPE)
+  num = output.stdout.read()
+  return np.convolve(x, np.ones(w), 'valid') / w
+
+data = np.array(num)
+
+print(moving_average(data,4))
+print ("Max number:",max(data), "\nMin number:",min(data))
