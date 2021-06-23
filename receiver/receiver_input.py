ad38bf738e2001537b862292c0205f3f29956dfa
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-23 22:17:05 +0530

    Update receiver_input.py

--- receiver/receiver_input.py
+++ receiver/receiver_input.py
@@ -4,10 +4,5 @@ import pandas as pd
 
 def bms_console_input():
     output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSSender.java'],stdout=subprocess.PIPE)
-    inputdata = output.stdout.read()  
+    inputdata = output.stdout.read()
     df = pd.DataFrame(inputdata)
-    temperature = df['temperature'].values
-    soc = df['soc'].values
-    charge_rate = df['chargeRate'].values
-    return temperature, soc, charge_rate
-
