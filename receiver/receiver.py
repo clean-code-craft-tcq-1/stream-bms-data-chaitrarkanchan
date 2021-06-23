b07992f37501d3b00162cf6da3474fc56ec72a20
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-23 20:01:21 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,14 +1,20 @@
+
+import sys
 import pandas as pd
-import numpy as np
-import subprocess
 
-
+def bms_console_input():
+    data = sys.stdin.readlines()
+    input_data = ''
+    
+    #get the streaming data from java console output in the pipeline
+    for line in data:
+        if 'temperature' in line:
+            input_data = line
+            
 def moving_average(x, w):
-  output = subprocess.Popen(['java','-cp', 'src/main/java/BMSStreamSender/BMSSender.java'],stdout=subprocess.PIPE)
-  num = output.stdout.read()
-  df = pd.DataFrame(num)
-  for i in range(0,df.shape[0]-2):
-    df['pandas_SMA_3'] = df.iloc[:,1].rolling(window=3).mean()
-    df.to_numpy().max()
-    df.to_numpy().min()
-    df.head()
+    df = pd.DataFrame(input_data)
+    for i in range(0,df.shape[0]-2):
+      df['pandas_SMA_3'] = df.iloc[:,1].rolling(window=3).mean()
+      df.to_numpy().max()
+      df.to_numpy().min()
+      df.head()
