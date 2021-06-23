b3e090c08cbf01b6eee8bb187a8b46c661405c8c
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-23 20:13:27 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -3,14 +3,14 @@ import sys
 import pandas as pd
 
 def bms_console_input():
-    data = sys.stdin.readlines()
-    input_data = ''
+    incoming_values = sys.stdin.readlines()
+    input = ''
     
-    #get the streaming data from java console output in the pipeline
-    for line in data:
-        if 'temperature' in line:
-            input_data = line
-    df = pd.DataFrame(input_data)
+    for range in incoming_values:
+        if 'temperature' in range:
+            input = range
+            
+    df = pd.DataFrame(input)
     for i in range(0,df.shape[0]-2):
       df['pandas_SMA_3'] = df.iloc[:,1].rolling(window=3).mean()
       df.to_numpy().max()
