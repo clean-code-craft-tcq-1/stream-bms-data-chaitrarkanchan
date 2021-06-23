617c2bd9430d270effea5620b56e521024bf1c3a
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-23 21:24:00 +0530

    Update receiver_functions.py

--- receiver/receiver_functions.py
+++ receiver/receiver_functions.py
@@ -3,16 +3,16 @@ charge_rate = []
 state_of_charge = []
 
 def get_inputs(temp, cor, soc):
-    temperature_range.append(temp)
-    charge_rate.append(cor)
-    state_of_charge.append(soc)
-    return temperature_range, charge_rate, state_of_charge
+   temperature_range.append(temp)
+   charge_rate.append(cor)
+   state_of_charge.append(soc)
+   return temperature_range, charge_rate, state_of_charge
 
 def bms_min(bms_param):
-    return min(bms_param)
+   return min(bms_param)
    
 def bms_max(bms_param):
-    return max(bms_param)
+   return max(bms_param)
   
 def bms_movingaverage(bms_param):
     length = len(bms_param)
