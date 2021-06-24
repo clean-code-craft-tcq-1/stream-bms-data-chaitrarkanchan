14eaae5000f5b84cb19e7c843602502810724213
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-24 21:12:53 +0530

    Update receiver_test.py

--- receiver/receiver_test.py
+++ receiver/receiver_test.py
@@ -8,7 +8,7 @@ class receiver_test(unittest.TestCase):
     
     self.assertTrue(receiver_functions.bms_min_temp([12,13,16,18,20]))
     self.assertTrue(receiver_functions.bms_max_temp([12,13,16,18,20]))
-    self.assertTrue(receiver_functions.bms_movingaverage_soc([12,13,16,18,20]))
+    self.assertTrue(receiver_functions.bms_movingaverage_soc([12,13,16,18,20],[11,12,15,17,19]))
     
 if __name__ == '__main__':
   unittest.main()
