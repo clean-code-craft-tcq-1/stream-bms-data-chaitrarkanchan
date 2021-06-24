d2899d3fbe0cf6b750cd9506f58beb4c10c374c1
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-24 21:22:25 +0530

    Update receiver_test.py

--- receiver/receiver_test.py
+++ receiver/receiver_test.py
@@ -7,10 +7,10 @@ class receiver_test(unittest.TestCase):
   def test_bms_param(self):
     
     self.assertTrue(receiver_functions.bms_min_temp([12,13,16,18,20]))
-    self.assertTrue(receiver_functions.bms_min_temp == 12)
+    self.assertTrue(receiver_functions.bms_min_soc([11,12,15,17,19]))    
     self.assertTrue(receiver_functions.bms_max_temp([12,13,16,18,20]))
-    self.assertTrue(receiver_functions.bms_max_temp == 20)
-    self.assertTrue(receiver_functions.bms_movingaverage_soc([12,13,16,18,20],[11,12,15,17,19]))
+    self.assertTrue(receiver_functions.bms_max_soc([11,12,15,17,19]))     
+    self.assertTrue(receiver_functions.bms_movingaverage([12,13,16,18,20],[11,12,15,17,19]))
     
 if __name__ == '__main__':
   unittest.main()
