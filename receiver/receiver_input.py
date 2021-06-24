c3f739ad67bab792bad88d4cf70e5bbe5ee6b7d4
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-24 21:08:11 +0530

    Update receiver_input.py

--- receiver/receiver_input.py
+++ receiver/receiver_input.py
@@ -5,10 +5,10 @@ from bms_limits import battery_allowedValues
 
 def bms_console_input():
   data = sys.stdin.readlines()    
-     for i in data:
-         bms_param_data = ast.literal_eval(data)
-         Temperature = bms_param_data['Temperature']
-         Soc = bms_param_data['Soc']
-         return Temperature, Soc
+  for i in data:
+    bms_param_data = ast.literal_eval(data)
+    Temperature = bms_param_data['Temperature']
+    Soc = bms_param_data['Soc']
+    return Temperature, Soc
         
     
