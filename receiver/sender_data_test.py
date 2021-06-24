94504545fde30c81fdafc8816c39bf3a2283d840
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-24 20:10:49 +0530

    Update sender_data_test.py

--- receiver/sender_data_test.py
+++ receiver/sender_data_test.py
@@ -2,4 +2,4 @@ import json
 import random
 
 for i in range(10):
-    print(json.dumps({"Temperature": random.randint(10,100),"Soc": random.randint(10,100)}))
+    print(json.dumps({"Temperature": random.randint(0,40),"Soc": random.randint(20,80)}))
