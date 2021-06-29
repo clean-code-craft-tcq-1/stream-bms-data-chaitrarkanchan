a30e54ba3ce941fd06148a37ad7b2fd157b08d24
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-06-29 15:24:20 +0530

    Update input.py

--- receiver/input.py
+++ receiver/input.py
@@ -4,8 +4,7 @@ def read_sender_inputs():
     try:
         input_value = input()
         input_list = json.loads(input_value)
-        print(input_list["Temperature"])
-        print(input_list["SOC"])
+        print(input_list)
         return input_list["Temperature"],input_list["SOC"]
     except EOFError:
         return None,None 
