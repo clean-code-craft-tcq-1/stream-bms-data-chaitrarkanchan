6bfbd753b9bc0317fd7f6b6e93671c76fca6fc55
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-07-01 16:55:59 +0530

    Update receiver_test.py

--- receiver/receiver_test.py
+++ receiver/receiver_test.py
@@ -4,10 +4,8 @@ import receiver_input
 
 
 class receiver_test(unittest.TestCase):  
-  Temperature,SOC = receiver_input.bms_console_readinput()
   def test_bms_param(self):
-    
-    self.assertTrue(receiver_functions.bms_min_temp([0,1,2,3,6]) == { 'min' :0})
+   
     self.assertTrue(receiver_functions.bms_min_temp([12,13,16,18,20]))
     self.assertTrue(receiver_functions.bms_min_soc([11,12,15,17,19]))    
     self.assertTrue(receiver_functions.bms_max_temp([12,13,16,18,20]))
