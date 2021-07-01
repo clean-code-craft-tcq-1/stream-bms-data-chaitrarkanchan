c1474bfca1b355a021bb98c1ad6c03cd5e365d5b
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-07-01 16:52:55 +0530

    Update receiver_input.py

--- receiver/receiver_input.py
+++ receiver/receiver_input.py
@@ -1,12 +1,9 @@
 import json
 
 def bms_console_readinput():
-   try:
         input_data = input()
         data_list = json.loads(input_data)
         return process_values(data_list)             
-   except EOFError:
-        return None, None
         
 def process_values(data_list):
    Temperature = data_list['Temperature']
